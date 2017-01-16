ddf8659d2ef656587e2dac8669ae0ee478bf7618
Nahush <nahush.farkande@coditas.com>
2017-01-16 17:57:39 +0530

    Saas configuration added

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,67 @@
+var webpack = require('webpack');
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var argv = process.argv || [];
+var PORT = 8080;
+var HOST = 'localhost';
+if (argv.includes('--port') > -1) {
+    PORT = argv[argv.indexOf('--port') + 1]
+}
 
-module.exports = {
-    entry: './src/index.tsx',
+if (argv.includes('--host') > -1) {
+    HOST = argv[argv.indexOf('--host') + 1]
+}
+
+var entryPoints = [
+    './src/index.tsx'
+];
+var plugins = [];
+var isProduction = argv.includes('--prod');
+var isRunningOnServer = argv.find(function (v) {
+    v.includes('webpack-dev-server');
+})
+
+if (isProduction) {
+    plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    )
+} else {
+
+    if (isRunningOnServer) {
+        entryPoints.push(
+            'webpack-dev-server/client?http:' + HOST + ':' + PORT,
+            'webpack/hot/only-dev-server'
+        )
+    }
+
+    plugins.push(
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('development')
+            }
+        })
+    )
+}
+
+plugins.push(
+    new HtmlWebpackPlugin({
+        template: 'index.ejs',
+        filename: 'index.html'
+    })
+);
+
+var config = {
+    entry: entryPoints,
     output: {
         filename: 'bundle.js',
         path: path.join(__dirname, 'dist')
@@ -17,7 +76,7 @@ module.exports = {
         ],
         loaders: [
             {test: /\.tsx?$/, exclude: /node_modules/, loaders: ['react-hot-loader/webpack', 'ts-loader']},
-            {test: /\.css$/, loader: 'style!css'},
+            {test: /\.scss$/, loaders: ['style-loader', 'css-loader', 'sass-loader']},
             {test: /.(png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/, loader: 'url-loader?limit=1024&name=fonts/[name].[ext]'},
             {test: /\.(jpg|jpeg|gif|png)$/, loader: 'url-loader?limit=10&mimetype=image/(jpg|jpeg|gif|png)&name=images/[name].[ext]'},
             {test: /\.json$/, loader: 'json-loader' }
@@ -28,8 +87,15 @@ module.exports = {
             filename: 'index.html',
             template: 'index.ejs'
         })
-    ],
-    devServer: {
-        contentBase: "dist"
+    ]
+}
+
+if (isRunningOnServer) {
+    config.devServer = {
+        contentBase: 'dist',
+        hot: true,
+        inline: true
     }
 }
+
+module.exports = config;
